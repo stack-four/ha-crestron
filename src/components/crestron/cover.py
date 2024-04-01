e15d291623d490fe8b36c1f365759206da04daab
Scott <scott@e-stateplanner.com>
2024-04-01 12:34:55 -0400

    minor updates

--- src/components/crestron/cover.py
+++ src/components/crestron/cover.py
@@ -103,31 +103,162 @@ class CrestronShadeEntity(CoverEntity):
     def current_cover_position(self):
         return self._shade.getPositionAsPercentage()
 
-    @property
-    def is_open(self):
-        return self._shade.is_open()
+    # @property
+    # def is_open(self):
+    #     return self._shade.isOpen()
 
-    @property
-    def is_opening(self):
-        return self._shade.is_opening()
+#TODO Add support for isOpening + isClosing ()
+    # @property
+    # def is_opening(self):
+    #     return self._shade.is_opening()
 
-    @property
-    def is_closing(self):
-        return self._shade.is_closing()
+    # @property
+    # def is_closing(self):
+    #     return self._shade.is_closing()
 
-    @property
-    def is_closed(self):
-        return self._shade.is_closed()
+    async def async_is_opening(self):
+        return self._shade.isOpening()
 
-    async def async_set_cover_position(self, **kwargs):
-        self._shade.set_analog(self._pos_join, int(kwargs["position"]) * 655)
+    async def async_is_open(self):
+        return self._shade.isClosed()
 
-    async def async_open_cover(self):
-        return self._shade.open()
+    async def async_is_closed(self):
+        return self._shade.isClosed()
 
-    async def async_close_cover(self):
-        return self._shade.close()
+    async def async_is_closing(self):
+        return self._shade.isClosing()
+    
+    async def async_set_cover_position(self, **kwargs)->None:
+        self._shade.setPosition(kwargs["position"])
 
-    async def async_stop_cover(self, **kwargs):
-        self._shade.set_digital(self._stop_join, 1)
-        call_later(self.hass, 0.2, self._shade.set_digital(self._stop_join, 0))
+    async def async_open_cover(self)->None:
+        self._shade.open()
+
+    async def async_close_cover(self)->None:
+        self._shade.close()
+
+    # @callback
+    # async def async_update_group_state(self) -> None:
+        # """Query all members and determine the light group state."""
+        # states = [
+        #     state
+        #     for entity_id in self._entity_ids
+        #     if (state := self.hass.states.get(entity_id)) is not None
+        # ]
+        # on_states = [state for state in states if state.state == STATE_ON]
+
+        # valid_state = self.mode(
+        #     state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE) for state in states
+        # )
+
+        # if not valid_state:
+        #     # Set as unknown if any / all member is unknown or unavailable
+        #     self._attr_is_on = None
+        # else:
+        #     # Set as ON if any / all member is ON
+        #     self._attr_is_on = self.mode(state.state == STATE_ON for state in states)
+
+        # self._attr_available = any(state.state != STATE_UNAVAILABLE for state in states)
+        # self._attr_brightness = reduce_attribute(on_states, ATTR_BRIGHTNESS)
+
+        # self._attr_hs_color = reduce_attribute(
+        #     on_states, ATTR_HS_COLOR, reduce=mean_tuple
+        # )
+        # self._attr_rgb_color = reduce_attribute(
+        #     on_states, ATTR_RGB_COLOR, reduce=mean_tuple
+        # )
+        # self._attr_rgbw_color = reduce_attribute(
+        #     on_states, ATTR_RGBW_COLOR, reduce=mean_tuple
+        # )
+        # self._attr_rgbww_color = reduce_attribute(
+        #     on_states, ATTR_RGBWW_COLOR, reduce=mean_tuple
+        # )
+        # self._attr_xy_color = reduce_attribute(
+        #     on_states, ATTR_XY_COLOR, reduce=mean_tuple
+        # )
+
+        # self._attr_color_temp_kelvin = reduce_attribute(
+        #     on_states, ATTR_COLOR_TEMP_KELVIN
+        # )
+        # self._attr_min_color_temp_kelvin = reduce_attribute(
+        #     states, ATTR_MIN_COLOR_TEMP_KELVIN, default=2000, reduce=min
+        # )
+        # self._attr_max_color_temp_kelvin = reduce_attribute(
+        #     states, ATTR_MAX_COLOR_TEMP_KELVIN, default=6500, reduce=max
+        # )
+
+        # self._attr_effect_list = None
+        # all_effect_lists = list(find_state_attributes(states, ATTR_EFFECT_LIST))
+        # if all_effect_lists:
+        #     # Merge all effects from all effect_lists with a union merge.
+        #     self._attr_effect_list = list(set().union(*all_effect_lists))
+        #     self._attr_effect_list.sort()
+        #     if "None" in self._attr_effect_list:
+        #         self._attr_effect_list.remove("None")
+        #         self._attr_effect_list.insert(0, "None")
+
+        # self._attr_effect = None
+        # all_effects = list(find_state_attributes(on_states, ATTR_EFFECT))
+        # if all_effects:
+        #     # Report the most common effect.
+        #     effects_count = Counter(itertools.chain(all_effects))
+        #     self._attr_effect = effects_count.most_common(1)[0][0]
+
+        # supported_color_modes = {ColorMode.ONOFF}
+        # all_supported_color_modes = list(
+        #     find_state_attributes(states, ATTR_SUPPORTED_COLOR_MODES)
+        # )
+        # if all_supported_color_modes:
+        #     # Merge all color modes.
+        #     supported_color_modes = filter_supported_color_modes(
+        #         cast(set[ColorMode], set().union(*all_supported_color_modes))
+        #     )
+        # self._attr_supported_color_modes = supported_color_modes
+
+        # self._attr_color_mode = ColorMode.UNKNOWN
+        # all_color_modes = list(find_state_attributes(on_states, ATTR_COLOR_MODE))
+        # if all_color_modes:
+        #     # Report the most common color mode, select brightness and onoff last
+        #     color_mode_count = Counter(itertools.chain(all_color_modes))
+        #     if ColorMode.ONOFF in color_mode_count:
+        #         if ColorMode.ONOFF in supported_color_modes:
+        #             color_mode_count[ColorMode.ONOFF] = -1
+        #         else:
+        #             color_mode_count.pop(ColorMode.ONOFF)
+        #     if ColorMode.BRIGHTNESS in color_mode_count:
+        #         if ColorMode.BRIGHTNESS in supported_color_modes:
+        #             color_mode_count[ColorMode.BRIGHTNESS] = 0
+        #         else:
+        #             color_mode_count.pop(ColorMode.BRIGHTNESS)
+        #     if color_mode_count:
+        #         self._attr_color_mode = color_mode_count.most_common(1)[0][0]
+        #     else:
+        #         self._attr_color_mode = next(iter(supported_color_modes))
+
+        # self._attr_supported_features = LightEntityFeature(0)
+        # for support in find_state_attributes(states, ATTR_SUPPORTED_FEATURES):
+        #     # Merge supported features by emulating support for every feature
+        #     # we find.
+        #     self._attr_supported_features |= support
+        # # Bitwise-and the supported features with the GroupedLight's features
+        # # so that we don't break in the future when a new feature is added.
+        # self._attr_supported_features &= SUPPORT_GROUP_LIGHT
+    
+    # async def async_close_cover(self, **kwargs: Any) -> None:
+    #     """Move the covers down."""
+    #     data = {ATTR_ENTITY_ID: self._covers[KEY_OPEN_CLOSE]}
+    #     await self.hass.services.async_call(
+    #         DOMAIN, SERVICE_CLOSE_COVER, data, blocking=True, context=self._context
+    #     )
+
+
+    # async def async_stop_cover(self, **kwargs: Any) -> None:
+    #     """Fire the stop action."""
+    #     data = {ATTR_ENTITY_ID: self._covers[KEY_STOP]}
+    #     await self.hass.services.async_call(
+    #         DOMAIN, SERVICE_STOP_COVER, data, blocking=True, context=self._context
+    #     )
+
+    # async def async_stop_cover(self, **kwargs):
+    #     self._shade.set_digital(self._stop_join, 1)
+    #     call_later(self.hass, 0.2, self._shade.set_digital(self._stop_join, 0))
